1a3ee9b96b63a8c6f2277eb50edaac2b3f990c3f
Miguel Cconcho Castellanos <migccast@gmail.com>
2022-12-17 19:12:18 -0500

    funcionalidad de reservar cita

--- scripts/datosCita.php
+++ scripts/datosCita.php
@@ -9,8 +9,9 @@ $codigo = $_POST["codigoAlumno"];
 $razon = $_POST["razon"];
 $codigoAlumno = $_SESSION["codigo"];
 $limite = 5;
+
 //Generar número de día
-$days=array(1=>"lunes",2=>"martes",3=>"miercoles", ,4=>"jueves",5=>"viernes",6=>"sabado",7=>"domingo");
+$days = array(1=>"lunes",2=>"martes",3=>"miercoles",4=>"jueves",5=>"viernes",6=>"sabado",7=>"domingo");
 $nroDia = array_search($dia,$days);
 //--------------------- Obtener fecha actual ---------------------
 //Establecer zona horario (Perú)
@@ -41,7 +42,7 @@ if ($var){
     date_add($fecha, date_interval_create_from_date_string($diference." days"));
     $newDate = date_format($fecha,"Y-m-d");
     //Conectar con la base de datos
-    include 'config.php';
+    include '../config.php';
     //Crear consulta
     $consulta = "INSERT INTO cita values('$newDate','$horaInicio','$horaFin','$codigoAlumno','PENDIENTE','$razon',null)";
     //Ejecutar consulta
