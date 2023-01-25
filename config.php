36dbfeaa67b683c2fed74952948d8f428a5f6cf2
STYersonH <102554781+STYersonH@users.noreply.github.com>
2023-01-25 12:30:29 -0500

    cambiando el servidor local en localhost

--- config.php
+++ config.php
@@ -2,6 +2,6 @@
 
 //Realizar conexión con la base de datos
 // En mi caso le coloco el puerto 3307 por que estoy trabajando en ese puerto, usualmente es 3306
-// $conexion = mysqli_connect("localhost", "root","","dbtutorias",3307);
-$conexion = mysqli_connect("localhost", "root","","dbtutorias");
+$conexion = mysqli_connect("localhost", "root","","dbtutorias",3307);
+//$conexion = mysqli_connect("localhost", "root","","dbtutorias");
 ?>
