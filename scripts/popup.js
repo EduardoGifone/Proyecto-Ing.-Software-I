8b85c156ec7fe295863422b66b51f90baa5a36a4
STYersonH <102554781+STYersonH@users.noreply.github.com>
2023-01-24 20:23:42 -0500

    revirtiendo commit

--- scripts/popup.js
+++ scripts/popup.js
@@ -7,9 +7,9 @@ const ShowDialogAll = (idContenedorAMostrar, Blur, BlurBackground) => {
     blur.classList.toggle('activate')
     var blur=document.getElementById(BlurBackground);
     blur.classList.toggle('activate');
-    /*var blur=document.getElementById('blurNavegacionBackground');
-    blur.classList.toggle('activate');*/
-    //para que asi no se agregue show al instante
+
+
+//para que asi no se agregue show al instante
     setTimeout(() => {
         document.getElementById(idContenedorAMostrar).classList.add('show')
     }, 1);
